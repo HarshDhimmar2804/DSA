fa55c02aac375b493f01c7054797a9b3d26423b0
HarshDhimmar2804 <harsh8487861901@gmail.com>
2024-07-11 12:26:23 +0530

    add stack

---
+++ Stack/stockSpan.java
@@ -0,0 +1,35 @@
+import java.util.Stack;
+
+public class stockSpan {
+
+  public static void stockSpan(int[] stock, int[] span) {
+    Stack<Integer> s = new Stack<>();
+    span[0] = 1;
+    s.push(0);
+    //in stack idx of previous highs
+
+    for (int i = 1; i < stock.length; i++) {
+      int currPrice = stock[i];
+      while (!s.isEmpty() && currPrice > stock[s.peek()]) {
+        s.pop();
+      }
+      if (s.isEmpty()) {
+        span[i] = i + 1;
+      } else {
+        int prevPrice = s.peek();
+        span[i] = i - prevPrice;
+      }
+      s.push(i);
+    }
+  }
+
+  public static void main(String[] args) {
+    int[] stock = { 100, 80, 60, 70, 60, 85, 100 };
+    int[] span = new int[stock.length];
+    stockSpan(stock, span);
+
+    for (int i = 0; i < span.length; i++) {
+      System.out.println(span[i] + " ");
+    }
+  }
+}
