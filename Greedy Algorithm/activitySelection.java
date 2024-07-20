87d91f9d217e545f4f145ae1026ea8fbcb64428e
HarshDhimmar2804 <harsh8487861901@gmail.com>
2024-07-20 10:16:12 +0530

    add greedy algorithm

---
+++ Greedy Algorithm/activitySelection.java
@@ -0,0 +1,41 @@
+import java.util.*;
+
+public class activitySelection {
+  public static void main(String[] args) {// O(nlog(n))
+    int start[] = { 1, 3, 0, 5, 8, 5 };
+    int end[] = { 2, 4, 6, 7, 9, 9 };
+
+    // sorting accroding to end
+    int activity[][] = new int[start.length][3];
+    for (int i = 0; i < activity.length; i++) {
+      activity[i][0] = i;
+      activity[i][1] = start[i];
+      activity[i][2] = end[i];
+    }
+
+    // lambda function->sortform //sort according to end time
+    Arrays.sort(activity, Comparator.comparingDouble(o -> o[2]));
+
+    // end time basic sorted
+    int maxAct = 0;
+    ArrayList<Integer> ans = new ArrayList<>();
+
+    // 1st activity selection
+    maxAct = 1;
+    ans.add(activity[0][0]);
+    int lastEnd = activity[0][2];
+    for (int i = 1; i < end.length; i++) {
+      if (activity[i][1] >= lastEnd) {
+        // activity select
+        maxAct++;
+        ans.add(activity[i][0]);
+        lastEnd = activity[i][2];
+      }
+    }
+    System.out.println("Max activity = " + maxAct);
+    for (int i = 0; i < ans.size(); i++) {
+      System.out.print("A" + ans.get(i) + " ");
+    }
+    System.out.println();
+  }
+}
